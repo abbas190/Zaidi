a90a434a10792e75cdc5f47e5f18d4e5add50af2
abbas190 <2767651@learn.vsb.bc.ca>
2023-09-11 13:10:06 -0700

    project

---
+++ untitled1/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {;
+            boolean canFly = true;
+
+            String name = "paul zaremba";
+            int spaceIndexPos = name.index0f(" ")
+
+            System.out.println(name.substring(0,4).length());
+        }
+    }
