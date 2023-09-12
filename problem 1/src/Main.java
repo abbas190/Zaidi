6bedb8b0cb9e54a786ae615632be5438d50fae5c
abbas190 <2767651@learn.vsb.bc.ca>
2023-09-12 12:46:59 -0700

    Problem 1

---
+++ problem 1/src/Main.java
@@ -0,0 +1,13 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int room = 113;
+                double num = 2.71828;
+                        String subject = "Computer Science";
+        System.out.println("This is room #"+ room);
+        System.out.println("e is close to"+ num);
+        System.out.println("I am learning a bit about"+ subject);
+
+    }
+}
