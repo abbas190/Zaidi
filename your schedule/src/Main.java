d7b4be59f11c8753e0bf3d362dc20d3d9f98cb40
abbas190 <2767651@learn.vsb.bc.ca>
2023-09-13 13:13:04 -0700

    schedule

---
+++ your schedule/src/Main.java
@@ -0,0 +1,32 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        String s1 = "English III";
+        String s2 = "Precalculus";
+        String s3 = "Music Theory";
+        String s4 = "Biotechnology";
+        String s5 = "Principles of Technology I";
+        String s6 = "Latin II";
+        String s7 = "AP US History";
+        String s8 = "Business Computer Information Systems";
+        String t1 = "Ms. Lapan";
+        String t2 = "Mrs. Gideon";
+        String t3 = "Mr. Davis";
+        String t4 = "Ms. Palmer";
+        String t5 = "Ms. Garcia";
+        String t6 = "Mrs. Barnett";
+        String t7 = "Ms. Johannessen";
+        String t8 = "Mr. James";
+        System.out.println("+-------------------------------------------------------------+");
+        System.out.println("| 1 |                                       |                 |");
+        System.out.println("| 2 |                                       |                 |");
+        System.out.println("| 3 |                                       |                 |");
+        System.out.println("| 4 |                                       |                 |");
+        System.out.println("| 5 |                                       |                 |");
+        System.out.println("| 6 |                                       |                 |");
+        System.out.println("| 7 |                                       |                 |");
+        System.out.println("| 8 |                                       |                 |");
+        System.out.println("+-------------------------------------------------------------+");
+}
+}
