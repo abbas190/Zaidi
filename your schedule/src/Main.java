9c44ce5bfbed6ddea8ccf539dea55181f11d0d66
abbas190 <2767651@learn.vsb.bc.ca>
2023-09-14 12:59:43 -0700

    schedule

--- your schedule/src/Main.java
+++ your schedule/src/Main.java
@@ -2,31 +2,31 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        String s1 = "English III";
-        String s2 = "Precalculus";
-        String s3 = "Music Theory";
-        String s4 = "Biotechnology";
-        String s5 = "Principles of Technology I";
-        String s6 = "Latin II";
-        String s7 = "AP US History";
-        String s8 = "Business Computer Information Systems";
-        String t1 = "Ms. Lapan";
-        String t2 = "Mrs. Gideon";
-        String t3 = "Mr. Davis";
-        String t4 = "Ms. Palmer";
-        String t5 = "Ms. Garcia";
-        String t6 = "Mrs. Barnett";
-        String t7 = "Ms. Johannessen";
-        String t8 = "Mr. James";
+        String s1 = "| 1 |                           English III";
+        String s2 = "| 2 |                           Precalculus";
+        String s3 = "| 3 |                          Music Theory";
+        String s4 = "| 4 |                         Biotechnology";
+        String s5 = "| 5 |            Principles of Technology I";
+        String s6 = "| 6 |                              Latin II";
+        String s7 = "| 7 |                         AP US History";
+        String s8 = "| 8 | Business Computer Information Systems";
+        String t1 = "Ms. Lapan       |";
+        String t2 = "Mrs. Gideon     |";
+        String t3 = "Mr. Davis       |";
+        String t4 = "Ms. Palmer      |";
+        String t5 = "Ms. Garcia      |";
+        String t6 = "Mrs. Barnett    |";
+        String t7 = "Ms. Johannessen |";
+        String t8 = "Mr. James       |";
         System.out.println("+-------------------------------------------------------------+");
-        System.out.println("| 1 |                                       |                 |");
-        System.out.println("| 2 |                                       |                 |");
-        System.out.println("| 3 |                                       |                 |");
-        System.out.println("| 4 |                                       |                 |");
-        System.out.println("| 5 |                                       |                 |");
-        System.out.println("| 6 |                                       |                 |");
-        System.out.println("| 7 |                                       |                 |");
-        System.out.println("| 8 |                                       |                 |");
+        System.out.println(s1 + " | " +t1);
+        System.out.println(s2 + " | " +t2);
+        System.out.println(s3 + " | " +t3);
+        System.out.println(s4 + " | " +t4);
+        System.out.println(s5 + " | " +t5);
+        System.out.println(s6 + " | " +t6);
+        System.out.println(s7 + " | " +t7);
+        System.out.println(s8 + " | " +t8);
         System.out.println("+-------------------------------------------------------------+");
 }
 }
