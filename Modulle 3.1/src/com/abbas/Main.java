60a48a548569fe85a4379888db871731c85f1657
abbas190 <144731994+abbas190@users.noreply.github.com>
2024-01-29 13:02:41 -0800

    Delete Modulle 3.1 directory

--- Modulle 3.1/src/com/abbas/Main.java
+++
@@ -1,8 +0,0 @@
-package com.abbas;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(Fibonacci.fibonacci(5));
-        System.out.println(Fibonacci.ans(5));
-    }
-}
